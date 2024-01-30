6885d5d25e5b319a01f392f02313fc24fe32915f
Christian <mail@christianhohenbild.de>
2024-01-30 14:26:30 +0100

    add leva
    needed to install leva v0.9.31 to prevent a unknown mime conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
+import { useControls, Leva } from 'leva'
 
 import './App.css'
 
@@ -8,14 +9,22 @@ import Cube from './Cube.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
+  const config = useControls('Controls', {
+    backgroundColor : '#ffffff'
+  })
+
   return (
+  <>
+    <Leva collapsed/>
     <Canvas
     camera={{ 
       position: [4.5, 2.5,5.5],
       fov: 40 }}  
     >
+      <color attach="background" args={[config.backgroundColor]} />
       <Cube />
     </Canvas>
+  </>
   )
 }
 
